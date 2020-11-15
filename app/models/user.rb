f64941ea49dd637a55635465002b335f62dc1690
onoderakosen89 <72384345+onoderakosen89@users.noreply.github.com>
2020-11-16 01:58:04 +0900

    Update application_controller.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :birthday, presence: true
   with_options presence: true do
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください' } 
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください' } 
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'カタカナを使用してください' } 
-    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'カタカナを使用してください' } 
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください' }
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '全角文字を使用してください' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'カタカナを使用してください' }
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'カタカナを使用してください' }
   end
-  
+
   has_many :products
   has_many :orders
 end
