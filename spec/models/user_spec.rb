a0eda8f23033ef65011a8bb050bbbed81e35f3ef
onoderakosen89 <72384345+onoderakosen89@users.noreply.github.com>
2020-11-16 11:52:11 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,54 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.nickname = ""
-      user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+      @user.nickname = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.email = ""
-      user.valid?
-      expect(user.errors.full_messages).to include("Email can't be blank")
+      @user.email = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
     end
     it 'passwordが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.password = ""
-      user.valid?
-      expect(user.errors.full_messages).to include("Password can't be blank")
+      @user.password = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
     end
     it 'family_nameが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.family_name = ""
-      user.valid?
-      expect(user.errors.full_messages).to include("Family name can't be blank")
+      @user.family_name = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Family name can't be blank")
     end
     it 'first_nameが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.first_name = ""
-      user.valid?
-      expect(user.errors.full_messages).to include("First name can't be blank")
+      @user.first_name = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name can't be blank")
     end
     it 'family_name_kanaが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.family_name_kana = ""
-      user.valid?
-      expect(user.errors.full_messages).to include("Family name kana can't be blank")
+      @user.family_name_kana = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Family name kana can't be blank")
     end
     it 'first_name_kanaが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.first_name_kana = ""
-      user.valid?
-      expect(user.errors.full_messages).to include("First name kana can't be blank")
+      @user.first_name_kana = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name kana can't be blank")
     end
     it 'birthdayが空では登録できない' do
-      user = FactoryBot.build(:user)
-      user.birthday = ""
-      user.valid?
-      expect(user.errors.full_messages).to include("Birthday can't be blank")
+      @user.birthday = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
   end
 end
