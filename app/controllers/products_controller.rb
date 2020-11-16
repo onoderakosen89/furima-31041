36b4411bef413ce852798d6099dc202ef46239b7
onoderakosen89 <72384345+onoderakosen89@users.noreply.github.com>
2020-11-16 12:21:28 +0900

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,4 @@
 class ProductsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
   before_action :authenticate_user!, except: [:index, :show]
   def index
   end
